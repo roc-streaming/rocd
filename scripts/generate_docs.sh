04bf18bc9bbb7d5636fe3f460a5b431c3adf8785
Victor Gaydov <victor@enise.org>
2026-06-22 23:39:56 +0900

    scripts: Add comment

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
+
+# This script is called from roc-streaming/roc-streaming.github.io CI
+# so that it doesn't need to know details about pixi & task.
+
 cd "$(dirname "$0")"/..
 pixi run task docs
